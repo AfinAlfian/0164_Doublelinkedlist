1145fbf4914129a35744ab1103d41b0b0daee631
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-04 20:23:28 +0700

    menampilkan menu pilihan

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -181,5 +181,21 @@ void searchData() {
 
 int main() 
 {
-
+    while (true)
+    {
+        try
+        {
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record to the list" << endl;
+            cout << "2. Delete a record from the list" << endl;
+            cout << "3. View all record in the ascending order of roll numbers" << endl;
+            cout << "4. View all record in the descending order of roll numbers;" << endl;
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+        }
+        catch (exception& e)
+        {
+            cout << "check for  the values entered." << endl;
+        }
+    }
 }
